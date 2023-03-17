d544428d54b87a81155d8606c0cf0d4f874e0b68
lea <lea.sagnier@gmail.com>
2023-03-17 10:59:41 +0100

    config ok

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("maze pathfinder")
+import {TOption, parseArgs} from "./parseArgs";
+
+// type ParseArgsResult = {
+//     mode : "server",
+//     httpPort : 80090,
+//     tcpPort : 5555,
+// } | {
+//     mode : "worker",
+//     serverSocket:"127.0.0.1:5555",
+//     nbThreads:12,
+// }
+
+async function main(){
+
+    const options: TOption | false = parseArgs(process.argv)
+    console.log(options)
+    
+    if(!options){
+        console.error(`unexpected arguments : ${process.argv.slice(2).join(" ")}`)
+        process.exit(-1)
+    }
+    
+    if(options.mode === "server"){
+        console.log("Run a server")
+    }else{
+        console.log("Run a worker")
+    }
+}
+main()
+
