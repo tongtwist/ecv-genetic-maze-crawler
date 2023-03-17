fc2b510a82e7b55bd3911fe42c7fadc695d4a1d1
RenauxLea <lea.renaux@mail-ecv.fr>
2023-03-17 09:55:57 +0100

    fetch command argument

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("maze pathfinder")
+import { parseArgs } from "node:util";
+
+const {
+  values: { name },
+} = parseArgs({
+  options: {
+    name: {
+      type: "string",
+      short: "n",
+    },
+    
+  },
+});
+
+console.log(`le nom est : ${name} `);
