98c7bf60071c009c8254c41cfe49c9e921694a6c
Quentin <quentin.moras@gmail.com>
2023-03-17 11:40:00 +0100

    add security

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-getParam(process.argv[2],process.argv[3],process.argv[4])
+getParam(process.argv[2],process.argv[3],parseInt(process.argv[4]))
 
 /*
 if (process.argv[2] === 'server') {
@@ -14,12 +14,22 @@ if (process.argv[2] === 'server') {
     var nbThread = parseInt(process.argv[4])
 }
 */
-function getParam(mode:string, arg1:string, arg2:string) {
+function getParam(mode:string, arg1:string, arg2:number) {
+    if (isNaN(arg2) == true || arg1 == "undefiend"){
+        console.log('erreur');
+        return ;
+    };
     if (mode === 'server') {
         console.log("run in server");
         var http_port = parseInt(arg1)
         var tcp_port = arg2
         
+        if(isNaN(http_port) == true || http_port < 0 || tcp_port < 0 ){
+            console.log('Valeur negative');
+            return ; 
+        }
+        
+
     }else{
         console.log("run in worker");
         var local = arg1
