66e7c370c22a7f97278d4b030159b8b9794df9e8
thomasdhollande <dhollande.thomas@gmail.com>
2023-03-17 11:21:08 +0100

    parseArgs dynamic

--- src/parseArgs.ts
+++ src/parseArgs.ts
@@ -14,16 +14,16 @@ export function parseArgs(args: string[]): TOption | false {
     const mode = args[2];
 
     if (mode === "server") {
-        const httpPort = 80090;
-        const tcpPort = 5555;
+        const httpPort = args[3] ? parseInt(args[3]) : 8090;
+        const tcpPort = args[4] ? parseInt(args[4]) : 5555;
         return {
             mode,
             httpPort,
             tcpPort
         };
     } else if (mode === "worker") {
-        const serverSocket = "127.0.0.1:5555";
-        const nbThreads = 12;
+        const serverSocket = args[3] ? args[3] : "127.0.0.1:5555";
+        const nbThreads = args[4] ? parseInt(args[4]) : 12;
         return {
             mode,
             serverSocket,
