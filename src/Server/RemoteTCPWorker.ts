ab20ea6f4152952e006795fd62ea4ac008bb7222
delacherie olivier <olivier28111999@hotmail.fr>
2023-04-14 11:22:42 +0200

    log msg worker

--- src/Server/RemoteTCPWorker.ts
+++ src/Server/RemoteTCPWorker.ts
@@ -38,7 +38,7 @@ export class RemoteTCPWorker implements IRemoteWorker {
         this._socket.on('data', (data: Buffer) => {
             const message = JSON.parse(data.toString())
             const handler = this._messageHandlers[message.type]
-            this._logger.log(`-> ${JSON.stringify(message)}`)
+            this._logger.log(`-> ${JSON.stringify(message.hostname)}`)
             if (handler) {
                 handler(message.data)
             }
