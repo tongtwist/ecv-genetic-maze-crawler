29cf478ede1ec53e5c3a1e45b30a78d691eb2bbe
iamdjoky <maude2413@hotmail.fr>
2023-04-14 11:11:58 +0200

    exercie TCP

---
+++ src/Server/RemoteTCPWorker.ts
@@ -0,0 +1,92 @@
+import { Socket, createConnection, AddressInfo } from "net"
+import { TJSON, ILogger, messageFromJSON, TMessageType,IBaseMessage, THealthMessage,StopMessage } from "../Common"
+import type { IRemoteWorker } from "./RemoteWorker.spec"
+
+export class RemoteTCPWorker implements IRemoteWorker {
+	private readonly _adr: AddressInfo | {}
+    private _connected: boolean = false
+    private _listening: boolean = false
+    private _messageHandlers: { [k: string]: (data: TJSON) => void } = {};
+    private _lastHealth?: IBaseMessage & THealthMessage;
+    private readonly _remoteWorkerLabel: string
+
+	constructor(
+		private readonly _logger: ILogger,
+    private readonly _socket: Socket
+
+	) {
+        this._adr = this._socket.address()
+        this._remoteWorkerLabel = `TCP Worker ${this._adrToString()}`
+    }
+
+  private _adrToString(): string {
+        return "family" in this._adr
+            ? `${this._adr.family}://${this._adr.address}:${this._adr.port}`
+            : ""
+  }
+
+  get lastHealth() {
+        return this._lastHealth;
+  }
+
+  private _bufferHandler(buf: Buffer): void {
+        const data = JSON.parse(buf.toString());
+        const retMessage = messageFromJSON(data);
+        if (retMessage.isSuccess) {
+          const message = retMessage.value!;
+          if (message.type in this._messageHandlers) {
+            this._logger.log(`-> Process ${message.type} message...`);
+            this._messageHandlers[message.type](data);
+          } else {
+            this._logger.log(`-> Skip "${message.type}" message type`);
+          }
+        } else {
+          this._logger.err(retMessage.error!.message);
+        }
+  }
+
+  setHealth(v: IBaseMessage & THealthMessage): void {
+        this._lastHealth = v;
+  }
+
+  listen() {
+		this._socket.on("data", this._bufferHandler.bind(this))
+		this._listening = true
+		this._logger.log(`Listening TCP Worker ${this._adrToString()} ...`)
+	}
+	send(data: TJSON): Promise<boolean> {
+		if (!this._connected || !this._socket) {
+			return Promise.resolve(false)
+		}
+		return new Promise((resolve: (v: boolean) => void) => {
+			this._socket!.write(JSON.stringify(data), "utf8", (err?: Error) => {
+				if (err) {
+					this._logger.log(`Error when emitting data to the server: ${err}`)
+					resolve(false)
+				} else {
+					resolve(true)
+				}
+			})
+		})
+	}
+
+  stop(): void {
+    this._messageHandlers = {};
+    this._listening = false;
+    this._logger.log(`Stop to listen TCP Worker`);
+    const msg = new StopMessage();
+    this.send(msg.toJSON());
+
+    if (this._socket) {
+      this._socket.end();
+    }
+}
+subscribe(type: TMessageType, handler: (data: TJSON) => void): boolean {
+  if (!this._listening) {
+    return false;
+  }
+  this._messageHandlers[type] = handler;
+  return true;
+}
+
+}
