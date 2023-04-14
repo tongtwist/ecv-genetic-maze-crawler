bb3a36bd4a1acb7ccc738c6dd732697cf01fce43
thomasdhollande <dhollande.thomas@gmail.com>
2023-04-14 11:42:35 +0200

    TCP Worker

---
+++ src/Server/RemoteTCPWorker.ts
@@ -0,0 +1,95 @@
+import net from "net";
+import {
+    TJSON,
+    TMessageType,
+    IBaseMessage,
+    messageFromJSON,
+    ILogger,
+    THealthMessage,
+    StopMessage,
+} from "../Common";
+import type { IRemoteWorker } from "./RemoteWorker.spec";
+import { Socket } from 'node:net';
+
+export class RemoteTCPWorker implements IRemoteWorker {
+    private _listening: boolean = false;
+    private _messageHandlers: { [k: string]: (data: TJSON) => void } = {};
+    private _lastHealth?: IBaseMessage & THealthMessage;
+
+    constructor(
+        private readonly _logger: ILogger,
+        private readonly _socket: net.Socket
+    ) { }
+
+    get lastHealth() {
+        return this._lastHealth;
+    }
+
+    private _messageHandler(data: TJSON) {
+        const retMessage = messageFromJSON(data);
+        if (retMessage.isSuccess) {
+            const message = retMessage.value!;
+            if (message.type in this._messageHandlers) {
+                this._logger.log(`-> Process ${message.type} message...`);
+                this._messageHandlers[message.type](data);
+            } else {
+                this._logger.log(`-> Skip "${message.type}" message type`);
+            }
+        } else {
+            this._logger.err(retMessage.error!.message);
+        }
+    }
+
+    setHealth(v: IBaseMessage & THealthMessage): void {
+        this._lastHealth = v;
+    }
+
+    listen() {
+        this._socket.on("data", (data: Buffer) => {
+            this._messageHandler(data.toString());
+        });
+        this._listening = true;
+        this._logger.log(`Listening TCP Worker ${this._socket.remoteAddress}:${this._socket.remotePort}...`);
+        this._socket.on("error", (err: Error) => {
+            this._logger.err(err.message);
+        });
+    }
+
+    stop() {
+        this._messageHandlers = {};
+        this._listening = false;
+        this._logger.log(
+            `Do not listen TCP Worker ${this._socket.remoteAddress}:${this._socket.remotePort} anymore`
+        );
+        const msg = new StopMessage();
+        this.send(msg.toJSON()).catch((err) => {
+            this._logger.err(err.message);
+        });
+        this._socket?.destroy();
+    }
+
+    async send(data: TJSON): Promise<boolean> {
+        return new Promise<boolean>((resolve, reject) => {
+            if (!this._socket?.writable) {
+                reject(new Error("Socket is not writable"));
+                return;
+            }
+            this._socket.write(JSON.stringify(data), (err) => {
+                if (err) {
+                    this._logger.err(err.message);
+                    reject(err);
+                    return;
+                }
+                resolve(true);
+            });
+        });
+    }
+
+    subscribe(type: TMessageType, handler: (data: TJSON) => void): boolean {
+        if (!this._listening) {
+            return false;
+        }
+        this._messageHandlers[type] = handler;
+        return true;
+    }
+}
--- src/Server/index.ts
+++ src/Server/index.ts
@@ -1,7 +1,8 @@
 import path from "node:path"
 import http from "node:http"
 import process from "node:process"
-import cluster, {Worker} from "node:cluster"
+import cluster, { Worker } from "node:cluster"
+import { Socket, createServer } from "node:net"
 import express from "express"
 import { DisconnectReason, Server as SocketIOServer, Socket as SocketIO } from "socket.io"
 import type { IMaze } from "./Maze.spec"
@@ -10,6 +11,7 @@ import { Maze } from "./Maze"
 import { IResult, TJSON, messageFromJSON, Logger, IBaseMessage, THealthMessage } from "../Common"
 import type { IRemoteWorker } from "./RemoteWorker.spec"
 import { RemoteIPCWorker } from "./RemoteIPCWorker"
+import { RemoteTCPWorker } from "./RemoteTCPWorker"
 
 export type TServerConfig = {
 	readonly httpPort: number
@@ -56,10 +58,12 @@ export function processBehavior(cfg: TServerConfig) {
 
 	httpServer.listen(cfg.httpPort, () => appLogger.log(`HTTP service listening on port ${cfg.httpPort}...`))
 
-	const remoteWorkers: {[id: string]: IRemoteWorker} = {}
+	const remoteWorkers: { [id: string]: IRemoteWorker } = {}
+	let exitHandling = false
 	cluster.on("online", (worker: Worker) => {
 		const workerID = `IPC${worker.id}`
-		remoteWorkers[workerID] = new RemoteIPCWorker(Logger.create(workerID), worker)
+		appLogger.log(`Remote local worker "${workerID}" is online`)
+		remoteWorkers[workerID] = new RemoteIPCWorker(Logger.create(`(from ${workerID}) `), worker)
 		remoteWorkers[workerID].listen()
 		remoteWorkers[workerID].subscribe("health", (data: TJSON) => {
 			const retHealthMessage = messageFromJSON(data) as IResult<IBaseMessage & THealthMessage>
@@ -68,12 +72,51 @@ export function processBehavior(cfg: TServerConfig) {
 				return
 			}
 			remoteWorkers[workerID].setHealth(retHealthMessage.value!)
+			appLogger.log(`Remote worker "${workerID}" is healthy`)
 		})
 		worker.on("disconnect", () => delete remoteWorkers[workerID])
-		appLogger.log(`Remote local worker "${workerID}" is online`)
-		
-		setTimeout(() => remoteWorkers[workerID].stop(), 10000)
 	})
+
+	const serverTCPSocket = createServer({
+		noDelay: true,
+		keepAlive: true,
+		keepAliveInitialDelay: 1000,
+	}, (socket: Socket) => {
+		const workerID = `TCP://${socket.remoteAddress}:${socket.remotePort}`
+		appLogger.log(`Remote local worker "${workerID}" is online`)
+		remoteWorkers[workerID] = new RemoteTCPWorker(Logger.create(`(from ${workerID}) `), socket)
+		remoteWorkers[workerID].listen()
+		remoteWorkers[workerID].subscribe("health", (data: TJSON) => {
+			const retHealthMessage = messageFromJSON(data) as IResult<IBaseMessage & THealthMessage>
+			if (retHealthMessage.isFailure) {
+				appLogger.err(retHealthMessage.error!.message)
+				return
+			}
+			remoteWorkers[workerID].setHealth(retHealthMessage.value!)
+			appLogger.log(`Remote worker "${workerID}" is healthy`)
+		})
+		socket.on("close", () => delete remoteWorkers[workerID])
+	})
+
+
+	async function stopServer(exitCode?: number): Promise<void> {
+		if (!exitHandling) {
+			exitHandling = true
+			appLogger.log("Server is stopping, stop all remote workers ...")
+			for (const workerID in remoteWorkers) {
+				await remoteWorkers[workerID].stop()
+			}
+			appLogger.log("Server stopped")
+		}
+		setTimeout(() => process.exit(exitCode ?? 0), 0)
+	}
+
+	// process.on("SIGINT", stopServer)
+	// process.on("exit", stopServer)
+
 	appLogger.log("Launch a local worker")
 	cluster.fork()
+
+	appLogger.log(`TCP service listening on port ${cfg.tcpPort}...`)
+	serverTCPSocket.listen(cfg.tcpPort)
 }
