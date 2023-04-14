7b6f45644eaabd0283b643438162235e4c61790d
Thirion Lucas <lucasthirion@live.fr>
2023-04-14 15:09:52 +0200

    Refactor

--- src/Server/RemoteTCPWorker.ts
+++ src/Server/RemoteTCPWorker.ts
@@ -11,54 +11,25 @@ import {
 import type { IRemoteWorker } from "./RemoteWorker.spec";
 import { AddressInfo } from "node:net";
 import { Socket } from "net";
+import { BaseRemoteWorker } from "./BaseRemoteWorker";
 
-export class RemoteTCPWorker implements IRemoteWorker {
+export class RemoteTCPWorker extends BaseRemoteWorker implements IRemoteWorker {
   private readonly _adr: AddressInfo | {};
   private readonly _remoteWorkerLabel: string;
-  private _connected: boolean = false;
-  private _messageHandlers: { [k: string]: (data: TJSON) => void } = {};
-  private _lastHealth?: IBaseMessage & THealthMessage;
 
   constructor(
-    private readonly _logger: ILogger,
+    protected readonly _logger: ILogger,
     private readonly _socket: Socket
   ) {
+    super(_logger);
     this._adr = this._socket.address();
     this._remoteWorkerLabel = `TCP Worker ${this._adrToString()}`;
-  }
-
-  private _cleanState() {
-    this._connected = false;
-    this._messageHandlers = {};
-  }
-
-  get lastHealth() {
-    return this._lastHealth;
   }
 
   private _adrToString(): string {
     return "family" in this._adr
       ? `${this._adr.family}://${this._adr.address}:${this._adr.port}`
       : "";
-  }
-
-  private _messageHandler(data: TJSON) {
-    const retMessage = messageFromJSON(data);
-    if (retMessage.isSuccess) {
-      const message = retMessage.value!;
-      if (message.type in this._messageHandlers) {
-        this._logger.log(`-> Process ${message.type} message...`);
-        this._messageHandlers[message.type](data);
-      } else {
-        this._logger.log(`-> Skip "${message.type}" message type`);
-      }
-    } else {
-      this._logger.err(retMessage.error!.message);
-    }
-  }
-
-  setHealth(v: IBaseMessage & THealthMessage): void {
-    this._lastHealth = v;
   }
 
   listen() {
@@ -116,14 +87,5 @@ export class RemoteTCPWorker implements IRemoteWorker {
         }
       });
     });
-  }
-
-  subscribe(type: TMessageType, handler: (data: TJSON) => void): boolean {
-    if (!this._connected || !this._socket) {
-      this._cleanState();
-      return false;
-    }
-    this._messageHandlers[type] = handler;
-    return true;
   }
 }
