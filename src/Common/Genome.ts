3ea3de2867abc95113f6d84f4a8cac5ce6895821
delacherie olivier <olivier28111999@hotmail.fr>
2023-04-14 16:24:08 +0200

    creation genome

---
+++ src/Common/Genome.ts
@@ -0,0 +1,39 @@
+import {IGenome, TGene} from "./Genome.spec";
+
+export class Genome implements IGenome {
+    genes: TGene[];
+    length: number;
+
+    constructor(genes: TGene[]) {
+        this.genes = genes;
+        this.length = genes.length;
+    }
+
+    public delete(idx?: number): void {
+        const index = idx || Math.floor(Math.random() * this.length);
+        this.genes.splice(index, 1);
+    }
+
+
+    public insert(idx?: number): void {
+        const index = idx || Math.floor(Math.random() * this.length);
+        this.genes.splice(index, 0, Math.floor(Math.random() * 4) as TGene);
+    }
+
+    public swap(idx?: number): void {
+        const index = idx || Math.floor(Math.random() * this.length);
+        this.genes[index] = Math.floor(Math.random() * 4) as TGene;
+    }
+
+    random(length: number){
+        this.genes = [];
+        for (let i = 0; i < length; i++) {
+            this.genes.push(Math.floor(Math.random() * 4) as TGene);
+        }
+        return new Genome(this.genes);
+    }
+
+    from(genes: TGene[]): Genome{
+        return new Genome(genes);
+    }
+}
